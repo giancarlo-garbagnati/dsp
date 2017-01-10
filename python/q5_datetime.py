15dff6acaa5785e138932f67a7c0c77e86d58077
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2017-01-10 13:47:29 -0800

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -5,12 +5,20 @@ date_start = '01-02-2013'
 date_stop = '07-28-2015'   
 
 from datetime import datetime
-datetime.strptime(date_start, format) - datetime.strptime(date_stop, format)
+
+format_a = "%m-%d-%Y"
+
+datetime.strptime(date_start, format_a) - datetime.strptime(date_stop, format_a)
 # datetime.timedelta(-937)
 
 ####b)  
 date_start = '12312013'  
-date_stop = '05282015'  
+date_stop = '05282015' 
+
+format_b = "%m%d%Y"
+
+datetime.strptime(date_start, format_b) - datetime.strptime(date_stop, format_b)
+# datetime.timedelta(-513)
 
 ####c)  
 date_start = '15-Jan-1994'  
