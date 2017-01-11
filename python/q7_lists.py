c0e37a567d14cdc1f0c0062a27dbeea8b8198e3d
giancarlo-garbagnati <giancarlo.garbagnati@gmail.com>
2017-01-10 22:00:19 -0800

    Update q7_lists.py

--- python/q7_lists.py
+++ python/q7_lists.py
@@ -15,7 +15,20 @@ def match_ends(words):
     >>> match_ends(['aaa', 'be', 'abc', 'hello'])
     1
     """
-    raise NotImplementedError
+    count = 0
+
+    l = len(words)
+    for i in range(l):
+        if len(words[i]) > 1:
+            if words[i][0] == words[i][-1]:
+                count = count + 1
+
+    return count
+    #raise NotImplementedError
+
+#print(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']))
+#print(match_ends(['', 'x', 'xy', 'xyx', 'xx']))
+#print(match_ends(['aaa', 'be', 'abc', 'hello']))
 
 
 def front_x(words):
@@ -32,7 +45,18 @@ def front_x(words):
     >>> front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark'])
     ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
     """
-    raise NotImplementedError
+
+    x_list = [x for x in words if x[0] == 'x']
+    remainder = [x for x in words if x[0] != 'x']
+
+    return sorted(x_list) + sorted(remainder)
+
+    #raise NotImplementedError
+
+
+#print(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']))
+#print(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']))
+#print(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']))
 
 
 def sort_last(tuples):
@@ -49,7 +73,18 @@ def sort_last(tuples):
     >>> sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)])
     [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
     """
-    raise NotImplementedError
+    
+    # couldn't figure out how to do it with list comprehension, so just went
+    #  with a lambda fxn
+    sorted_tuples = sorted(tuples, key = lambda x: x[-1])
+
+    return sorted_tuples
+
+    #raise NotImplementedError
+
+#print(sort_last([(1, 3), (3, 2), (2, 1)]))
+#print(sort_last([(2, 3), (1, 2), (3, 1)]))
+#print(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]))
 
 
 def remove_adjacent(nums):
@@ -68,7 +103,58 @@ def remove_adjacent(nums):
     >>> remove_adjacent([])
     []
     """
-    raise NotImplementedError
+
+    # recursive soln :(
+    # if there's a list comprehension / filter+lambda soln I couldn't see it
+
+    def divide_and_compare(nums_list):
+        # returns just the list if the length is less than 2
+        if len(nums_list) < 2:
+            return nums_list
+
+        # if the list has two elements in it, compare and if they are the same, 
+        #  delete the last element
+        if len(nums_list) == 2:
+            if nums_list[0] == nums_list[1]:
+                cleaned = nums_list
+                cleaned.pop(1)
+                return cleaned
+            else:
+                return nums_list
+        
+        # divides the list into a list of the last element, and a list of all
+        #  the elements before the last
+        last = [nums_list[-1]]
+        front = nums_list
+        front.pop(-1)
+
+        # calls d_a_c on the all the elements but the last
+        front = divide_and_compare(front)
+
+        # recombine the two lists
+        combined = front + last
+
+        # another check to see if the last and second to last elements are the
+        #  same, and removes the last element if so
+        if combined[-1] == combined[-2]:
+            combined.pop(-1)
+
+        return combined
+
+    '''
+    cleaned_nums = [nums[0]] + nums
+    cleaned_nums = [ x for x in cleaned_nums[1:] 
+                    if cleaned_nums[x] != cleaned_nums[x-1] ]
+    return cleaned_nums
+    '''
+    return divide_and_compare(nums)
+
+    #raise NotImplementedError
+
+#print(remove_adjacent([1, 2, 2, 3]))
+#print(remove_adjacent([2, 2, 3, 3, 3]))
+#print(remove_adjacent([3, 2, 3, 3, 3]))
+#print(remove_adjacent([]))
 
 
 def linear_merge(list1, list2):
@@ -85,4 +171,34 @@ def linear_merge(list1, list2):
     >>> linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb'])
     ['aa', 'aa', 'aa', 'bb', 'bb']
     """
-    raise NotImplementedError
+    i = 0
+    j = 0
+    list3 = []
+
+    # two indices to traverse the two lists, adds the lesser element of each
+    #  comparison then iterating the list with the lesser element, this
+    #  continues until one list runs out
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            list3.append(list1[i])
+            i = i + 1
+        else:
+            list3.append(list2[j])
+            j = j + 1
+
+    # adds the remainder of whatever list still has elements leftover
+    if i == len(list1):
+        list3 = list3 + list2[j:]
+    else:
+        list3 = list3 + list1[i:]
+
+    return list3
+
+
+    #raise NotImplementedError
+
+
+
+#print(linear_merge(['aa', 'xx', 'zz'], ['bb', 'cc']))
+#print(linear_merge(['aa', 'xx'], ['bb', 'cc', 'zz']))
+#print(linear_merge(['aa', 'aa'], ['aa', 'bb', 'bb']))
